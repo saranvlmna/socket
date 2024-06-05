8241b7acf993b601c6309c4d5f8ae567a1fdefdf
Saranjith <saranjith@metrictreelabs.com>
2024-06-05 15:56:10 +0530

    push

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const { createServer } = require("node:http");
 const socketService = require("./src/socket.service");
 const port = 7001;
+var cors = require("cors");
 var app = express();
-
+app.use(cors());
 const server = createServer(app);
 server.listen(port, () => {
   console.log(`socket service listening at http://localhost:${port}/ 🤡`);
--- src/socket.service.js
+++ src/socket.service.js
@@ -1,13 +1,38 @@
-const { Server } = require("socket.io");
-
 module.exports = async (server) => {
   try {
-    const io = new Server(server);
-
+    const io = require("socket.io")(server, {
+      cors: {
+        origin: "*",
+      },
+    });
+    let activeUsers = [];
     io.on("connection", (socket) => {
       console.log("a user connected", socket.id);
-      socket.on("disconnect", () => {
+      socket.on("disconnect_user", (msg) => {
         console.log("user disconnected", socket.id);
+        if (activeUsers.includes(msg.userName)) {
+   
+        }
+        io.emit("activeUsers", {
+          data: activeUsers,
+        });
+      });
+
+      socket.on("sendMessage", (msg) => {
+        io.emit("responseMessage", {
+          name: msg.name,
+          text: msg.text,
+          id: msg.id,
+        });
+      });
+
+      socket.on("newUser", (msg) => {
+        if (!activeUsers.includes(msg.userName)) {
+          activeUsers.push(msg.userName);
+        }
+        io.emit("activeUsers", {
+          data: activeUsers,
+        });
       });
     });
   } catch (error) {
